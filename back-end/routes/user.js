624ea745ffceb786ee73710d26e8f5f4244e1539
Zhu Chu <mystery490815101@gmail.com>
2017-08-17 17:54:30 +0800

    add auth check

--- back-end/routes/user.js
+++ back-end/routes/user.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const {user} = require('../controller');
+const {assert} = require('../lib');
 
-router.post('/', user.regist);
-router.post('/login', user.login);
-router.get('/logout', user.logout);
-router.put('/user/self', user.edit);
+router
+  .post('/', user.regist)
+  .post('/login', user.login);
+
+/**
+ * access control - all the apis below require a logged-in state
+ */
+router.use((req, res, next) => {
+  assert(res.locals.user, 'not logged in');
+});
+
+router
+  .get('/logout', user.logout)
+  .put('/user/self', user.edit);
 
 module.exports = router;

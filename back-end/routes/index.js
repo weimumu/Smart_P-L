a1b92549bfb7d6ec71358b0e8e63124803ca4f93
Zhu Chu <mystery490815101@gmail.com>
2017-08-20 18:49:48 +0800

    api whitelist

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,6 +1,33 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const {assert} = require('../lib');
+
+/**
+ * only routers in the whiteList are accessible without a logged-in state
+ */
+router.use((req, res, next) => {
+  assert(res.locals.user || match(req), 'not logged in');
+  next();
+});
 
 router.use('/user', require('./user'));
 
 module.exports = router;
+
+const whiteList = [
+  {
+    method: 'POST',
+    path: '/user'
+  },
+  {
+    method: 'POST',
+    path: '/user/login'
+  }
+];
+
+function match (req) {
+  for (const config of whiteList) {
+    if (config.method === req.method && config.path === req.path) return true;
+  }
+  return false;
+}
--- back-end/routes/user.js
+++ back-end/routes/user.js
@@ -1,21 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const {user} = require('../controller');
-const {assert} = require('../lib');
 
 router
   .post('/', user.regist)
-  .post('/login', user.login);
-
-/**
- * access control - all the APIs below require a logged-in state
- */
-router.use((req, res, next) => {
-  assert(res.locals.user, 'not logged in');
-  next();
-});
-
-router
+  .post('/login', user.login)
   .get('/logout', user.logout)
   .put('/self', user.edit)
   .get('/self', user.getSelf);

814f0f9e3f4416b3b30e6fb26bbe88e3787daf1f
weimumu <934657014@qq.com>
2017-08-22 14:19:53 +0800

    好友部分90%

--- front-end/src/components/business/partner.vue
+++ front-end/src/components/business/partner.vue
@@ -18,13 +18,13 @@
       <div class="rightContent">
         <div class="partLeft">
           <div>
-            <img src="/static/business/partner/icon_search.png" class="search">
-            <input type="text" class="searchInput"> 
+            <img src="/static/business/partner/icon_search.png" class="search" @click="searchFriend">
+            <input type="text" class="searchInput" v-model="searchContent" @keyup.enter="searchFriend" placeholder="请输入公司全称"> 
           </div>
           <div class="list">
              <mu-list>
               <li v-for="items in getFriends" style="list-style-type:none;">
-                <mu-list-item :title=items.comName :key="items.comName" :describeText=items.comField :titleClass="{listFont: true}" :describeTextClass="{listFont1: true}">
+                <mu-list-item @click="showMes(items._id)" :title=items.comName :key="items.comName" :describeText=items.comField :titleClass="{listFont: true}" :describeTextClass="{listFont1: true}">
                   <mu-avatar :src=items.url slot="leftAvatar" :size="30"/>
                 </mu-list-item>
                 <mu-divider/>
@@ -32,7 +32,62 @@
             </mu-list> 
           </div>
         </div>
-        <div class="partRight"></div>
+        <div class="partRight" v-if="initActive" :style="{marginTop : '100px'}">
+          <span :style="{color: '#d6a12c', fontSize: '30px'}">智产智融</span>
+          <div :style="{marginTop : '30px'}">
+            <img src="/static/logo.png" :style="{width: '70px'}">
+          </div>
+        </div>
+        <div class="partRight" v-if="searchActive === true && !initActive">
+          <img src="/static/business/partner/boy.png">
+          <div class="content">
+            <span>{{searchResult.comName}}</span>
+            <div class="contentMes">
+              <div class="left">
+                <p>成立时间：{{searchResult.comTime}}</p>
+                <p>注册资本：200美元</p>
+                <p>注册地址：{{searchResult.comRegistAddresss}}</p>
+                <p>办公地址：{{searchResult.comWorkAddresss}}</p>
+              </div>
+              <div class="right">
+                <p>公司电话：{{searchResult.comPhone}}</p>
+                <p>所属行业：{{searchResult.comField}}</p>
+                <p>主营产品：{{searchResult.comProduct}}</p>
+                <p>简介：{{searchResult.comIntro}}</p>
+              </div>
+            </div>
+          </div>
+          <button class="addFriend" @click="addFriend">添加好友</button>
+        </div>
+        <div class="partRight" :style="{marginTop: '20px'}" v-if="searchActive === false && !initActive">
+          <img src="/static/business/partner/boy.png">
+          <div class="content">
+            <span>{{comMes.comName}}</span>
+            <div class="contentMes">
+              <div class="left" :style="{marginTop : '20px'}">
+                <p>成立时间：{{comMes.comTime}}</p>
+                <p>注册资本：200美元</p>
+                <p>注册地址：{{comMes.comRegistAddresss}}</p>
+                <p>办公地址：{{comMes.comWorkAddresss}}</p>
+                <p>公司电话：{{comMes.comPhone}}</p>
+                <p>所属行业：{{comMes.comField}}</p>
+                <p>主营产品：{{comMes.comProduct}}</p>
+                <p>简介：{{comMes.comIntro}}</p>
+              </div>
+              <div class="right" :style="{marginTop : '20px'}">
+                <p :style="{color: '#d6a12c', fontSize: '18px'}">联系人信息</p>
+                <p>姓名：{{searchResult.comPhone}}</p>
+                <p>职务：{{searchResult.comField}}</p>
+                <p>手机：{{searchResult.comProduct}}</p>
+                <p>电子邮箱：{{searchResult.comProduct}}</p>
+                <p>QQ号码：{{searchResult.comIntro}}</p>
+                <p>公司电话：{{searchResult.comIntro}}</p>
+              </div>
+            </div>
+          </div>
+          <button class="delFriend special" @click="delFriend(comMes._id)">删除好友</button>
+          <button class="delFriend" @click="delFriend">交易记录</button>
+        </div>
       </div>
     </div>
     <div class="moments" v-if="show.moments">
@@ -49,24 +104,33 @@ export default {
   data () {
     return {
       isActive: true,
+      searchContent: '',
+      searchActive: false,
+      initActive: true,
+      searchResult: {
+        _id: '',
+        comName: '',
+        comTime: '',
+        comRegistAddresss: '',
+        comWorkAddresss: '',
+        comPhone: '',
+        comField: '',
+        comProduct: '',
+        comIntro: ''
+      },
+      comMes: {
+      },
       show: {
         friend: true,
         moments: false,
         news: false
       },
       friends: [
-        {
-          url: '/static/business/partner/boy.png',
-          comName: '华为公司',
-          comField: '互联网'
-        },
-        {
-          url: '/static/business/partner/boy.png',
-          comName: '华为公司',
-          comField: '互联网'
-        }
       ]
     };
+  },
+  async created () {
+    this.initData();
   },
   methods: {
     goTo (mes) {
@@ -77,11 +141,72 @@ export default {
           this.show[key] = false;
         }
       }
+    },
+    async showMes (id) {
+      this.initActive = false;
+      this.searchActive = false;
+      let res;
+      try {
+        res = await this.$http.get('/api/user/' + id);
+        this.comMes = res.data;
+        console.log(this.comMes);
+      } catch (e) {
+
+      }
+    },
+    async searchFriend () {
+      let res = await this.$http.get('/api/user/search?q=' + this.searchContent);
+      if (res.data.length) {
+        this.searchActive = true;
+        this.initActive = false;
+        for (var key in this.searchResult) {
+          this.searchResult[key] = res.data[0][key];
+        }
+      } else {
+        this.$store.commit('info', '暂时无此公司记录');
+      }
+    },
+    async addFriend () {
+      try {
+        await this.$http.post('/api/friend/request?to=' + this.searchResult._id);
+        this.$store.commit('info', '请求发送成功，等待回复!');
+      } catch (e) {
+        this.$store.commit('info', '已发送，请耐心等待回复');
+      }
+    },
+    async delFriend (mes) {
+      try {
+        await this.$http.delete('/api/friend/' + mes);
+        this.$store.commit('info', '成功删除好友');
+        this.initData();
+      } catch (e) {
+        this.$store.commit('info', '网络错误');
+      }
+    },
+    async initData () {
+      let res;
+      try {
+        res = await this.$http.get('/api/friend/list');
+        this.friends = res.data;
+        for (let i = 0; i < this.friends.length; i++) {
+          this.friends[i].url = '/static/business/partner/boy.png';
+        }
+      } catch (e) {
+
+      }
     }
   },
   computed: {
     getFriends () {
-      return this.friends;
+      var data = this.friends;
+      var that = this;
+      if (this.searchContent) {
+        data = data.filter(function (items) {
+          console.log(items);
+          return items['comName'].indexOf(that.searchContent) >= 0;
+        });
+      }
+      return data;
     }
   }
 };
@@ -131,21 +256,25 @@ export default {
   width: 796px;
 }
 .rightContent{
+  position: relative;
   margin-top: 50px;
   height: 450px;
   width: 796px;
-  background: red;
   background: url("/static/business/partner/bg_friend.png");
   background-size: 100% 100%;
   .partLeft {
+    display: inline-block;
     position: relative;
     height: 450px;
     width: 195px;
     .search{
       margin-top: 15px;
       margin-left: 16px;
+      cursor: pointer;
     }
     .searchInput{
+      padding-left: 5px;
+      font-size: 11px;
       position: absolute;
       right: 10px;
       top: 10.5px;
@@ -161,6 +290,64 @@ export default {
       height: 400px;
       margin-left: 6px;
       margin-right: 2px;
+    }
+  }
+  .partRight{
+    position: absolute;
+    display: inline-block;
+    width: 590px;
+    height: auto;
+    text-align: center;
+    margin-top:50px;
+    img{
+      width: 50px;
+    }
+    .content>span{
+      color: #d6a12c;
+      font-size: 25px;
+      position: relative;
+      top: 10px;
+    }
+    .contentMes {
+      line-height: 1;
+      width: 500px;
+      margin: 0 auto;
+      font-size: 15px;
+      .left, .right{
+        text-align: left;
+        margin-top: 40px;
+        padding-left: 45px;
+        width: 50%;
+      }
+      .left{
+        float: left;
+      }
+      .right{
+        float: right;
+      }
+    }
+    .addFriend, .delFriend{
+      width: 112px;
+      height: 41px;
+      border: 0;
+      color: #d6a12c;
+      font-size: 16px;
+      position: relative;
+      top: 25px;
+      outline: none;
+      background: url("/static/business/partner/addFriend.png");
+      background-size: 100% 100%;
+    }
+    .delFriend{
+      width: 100px;
+      height: 37px;
+      position: relative;
+      right: 150px;
+      top: 25px;
+      font-size: 15px;
+    }
+    .special{
+      margin-right: 50px;
     }
   }
 }
